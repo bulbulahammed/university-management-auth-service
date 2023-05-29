206c2d839fed8d4b6a59f3178f285b976aa6593a
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-05-29 18:53:50 +0600

    [DONE] Setup Express, Mongoose, cors and Parser1

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+// usr cors
+app.use(cors());
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Testing
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi this is University Server✌");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function DBconnection() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("🛢 Database connected successfully ✔");
+    app.listen(config.port, () => {
+      console.log(`University server listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Failed to connect to database 🐞❌");
+  }
+}
+DBconnection();
