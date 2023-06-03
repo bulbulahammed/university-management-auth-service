83bba5c7147e6b459a8e5c9ae29141946a66212e
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-06-03 17:20:43 +0600

    Database connection issue

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { errorLogger, logger } from './shared/logger'
 
 async function DBconnection() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('🛢 Database connected successfully ✔')
+    logger.info('🛢 Database connected successfully ✔')
     app.listen(config.port, () => {
-      console.log(`University server listening on port ${config.port}`)
+      logger.info(`University server listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('Failed to connect to database 🐞❌')
+    errorLogger.error('Failed to connect to database 🐞❌')
   }
 }
 DBconnection()
---
+++ src/shared/logger.ts
@@ -0,0 +1,34 @@
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
+
+// Custom log formate
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+
+const errorLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+
+export { logger, errorLogger }
