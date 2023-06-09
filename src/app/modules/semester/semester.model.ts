2f0cf4d5037d4e1664aa50779e0dab3114e3a7cc
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-06-09 17:59:38 +0600

    [DONE] Same Year Same semester issue

--- src/app/modules/semester/semester.model.ts
+++ src/app/modules/semester/semester.model.ts
@@ -1,4 +1,6 @@
+import status from 'http-status';
 import { Schema, model } from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import {
   academicSemesterCodes,
   academicSemesterMonths,
@@ -37,6 +39,20 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   }
 );
+
+// Handle Same Year Same Semester issue
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(status.CONFLICT, 'Academic Semester is Already Exist!');
+  }
+  // next is not from Express it's from mongoose hook
+  next();
+});
+
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
   academicSemesterSchema
