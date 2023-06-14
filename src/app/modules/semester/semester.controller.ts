5b6c25dd9735f93cf576d3583d0df0bbdff932ee
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-06-14 18:16:05 +0600

    [DONE] Optimize query, Implement paginatin and Dynamic Sorting

--- src/app/modules/semester/semester.controller.ts
+++ src/app/modules/semester/semester.controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { IAcademicSemester } from './semester.interface';
 import { AcademicSemesterService } from './semester.service';
 
+// Create Semester
 const createSemester = catchAsync(async (req: Request, res: Response) => {
   const { ...academicSemesterData } = req.body;
   const result = await AcademicSemesterService.createSemester(
@@ -17,6 +21,23 @@ const createSemester = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+// Get All Semester
+const getAllSemesters = catchAsync(async (req: Request, res: Response) => {
+  const paginationOptions = pick(req.query, paginationFields);
+
+  const result = await AcademicSemesterService.getAllSemesters(
+    paginationOptions
+  );
+  sendResponse<IAcademicSemester[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Retrieved Successfully!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicSemesterController = {
   createSemester,
+  getAllSemesters,
 };
--- src/app/modules/semester/semester.service.ts
+++ src/app/modules/semester/semester.service.ts
@@ -1,9 +1,14 @@
 import httpStatus from 'http-status';
+import { SortOrder } from 'mongoose';
 import ApiError from '../../../errors/ApiError';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/paginations';
 import { academicSemesterTitleCodeMapper } from './semester.constant';
 import { IAcademicSemester } from './semester.interface';
 import { AcademicSemester } from './semester.model';
 
+// Create Semester Service
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
@@ -15,6 +20,37 @@ const createSemester = async (
   return result;
 };
 
+// Get All Semester Service
+const getAllSemesters = async (
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IAcademicSemester[]>> => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const sortCondition: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortCondition[sortBy] = sortOrder;
+  }
+
+  const result = await AcademicSemester.find()
+    .sort(sortCondition)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await AcademicSemester.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicSemesterService = {
   createSemester,
+  getAllSemesters,
 };
