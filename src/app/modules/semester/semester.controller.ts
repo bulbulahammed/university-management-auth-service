be655adb1f00d42fbea090430f701b9177bb539e
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-06-14 11:47:27 +0600

    user Creation error solve

--- src/app/modules/semester/semester.controller.ts
+++ src/app/modules/semester/semester.controller.ts
@@ -1,24 +1,21 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './semester.service';
 
-const createSemester = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { ...academicSemesterData } = req.body;
-    const result = await AcademicSemesterService.createSemester(
-      academicSemesterData
-    );
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Semester Created Successfully!',
-      data: result,
-    });
-    next();
-  }
-);
+const createSemester = catchAsync(async (req: Request, res: Response) => {
+  const { ...academicSemesterData } = req.body;
+  const result = await AcademicSemesterService.createSemester(
+    academicSemesterData
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Created Successfully!',
+    data: result,
+  });
+});
 
 export const AcademicSemesterController = {
   createSemester,
