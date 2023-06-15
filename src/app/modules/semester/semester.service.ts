9c53849165a5f46ece6740c470e87b9467e450fd
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-06-15 09:30:56 +0600

    [DONE] Apply Where condition to get all semester data, no filtering, no sorting, no pagination

--- src/app/modules/semester/semester.service.ts
+++ src/app/modules/semester/semester.service.ts
@@ -64,7 +64,12 @@ const getAllSemesters = async (
     sortCondition[sortBy] = sortOrder;
   }
 
-  const result = await AcademicSemester.find({ $and: andCondition })
+  const whereConditions = andCondition.length > 0 ? { $and: andCondition } : {};
+
+  // TODO: Delete this line after apply Where Conditions
+  // const result = await AcademicSemester.find({ $and: andCondition })
+
+  const result = await AcademicSemester.find(whereConditions)
     .sort(sortCondition)
     .skip(skip)
     .limit(limit);
